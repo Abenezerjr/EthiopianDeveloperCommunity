10d4dedd0e2a963b68b24156e26c25f25336dd86
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-03-25 22:55:31 +0300

    fix bug

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from django.contrib import messages
 from .utils import searchProject, paginationProjects
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def projects(request):
